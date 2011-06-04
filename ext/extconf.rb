b3d35737dea199133c15754751adff77756e5752
Oliver Whyte <oawhyte@gmail.com>
2011-06-04 12:57:37 -0700

    Added -fcommon to CFLAGS in extconf.rb -- avoids duplicate symbols error on compilation

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -5,7 +5,7 @@ dir_config('peg_markdown')
 $objs = %w[markdown.o markdown_lib.o markdown_output.o markdown_parser.o]
 
 if pkg_config = find_executable('pkg-config')
-  $CFLAGS = `#{pkg_config} --cflags glib-2.0`
+  $CFLAGS = "-fcommon "+`#{pkg_config} --cflags glib-2.0`
   $LDFLAGS = `#{pkg_config} --libs glib-2.0`
 else
   fail "glib2 not found"
